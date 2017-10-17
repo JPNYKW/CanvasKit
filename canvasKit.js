3af0bf4eedf8be354be0511a781a1ce23ae910fb
JPNYKW <31243896+JPNYKW@users.noreply.github.com>
2017-10-17 15:10:01 +0900

    Add files via upload

---
+++ canvasKit.js
@@ -0,0 +1,27 @@
+function drawDot(cont,x,y,size,color){
+	cont.beginPath();
+	cont.fillStyle=color;
+	cont.arc(x,y,size,Math.PI*2,false);
+	cont.fill();
+}
+
+function drawLine(cont,x,y,x1,y1,bold,color){
+	cont.beginPath();
+	cont.lineWidth=bold;
+	cont.strokeStyle=color;
+	cont.moveTo(x,y);
+	cont.lineTo(x1,y1);
+	cont.closePath();
+	cont.stroke();
+}
+
+function drawGrid(cont,x,y,size,width,height,bold,color){
+	baseX=x-width/2*size;
+	baseY=y-height/2*size;
+	for(i=0;i<width+1;i++){
+		drawLine(cont,baseX,baseY+i*size,baseX+width*size,baseY+i*size,bold,color);
+	}
+	for(i=0;i<width+1;i++){
+		drawLine(cont,baseX+i*size,baseY,baseX+i*size,baseY+height*size,bold,color);
+	}
+}
---
+++ main.js
@@ -0,0 +1,21 @@
+window.onload=()=>{
+	var canv,cont;
+	var doc=document;
+	canv=doc.getElementById('drawArea');
+	cont=canv.getContext('2d');
+	canv.style.backgroundColor='#2C2C2C';
+	canv.width=640;
+	canv.height=640;
+	cont.clearRect(0,0,canv.width,canv.height);
+	
+	//CanvasKit Functions
+	
+	drawDot(cont,320,320,8,'#F3F3F3');
+
+	drawLine(cont,50,128,590,128,3,'#F3F3F3');
+	drawLine(cont,50,512,590,512,3,'#F3F3F3');
+
+	drawGrid(cont,320,320,32,5,5,3,'#F3F3F3');
+	drawGrid(cont,320,320,640,1,1,32,'#C5C5C5');
+	drawGrid(cont,320,320,640,1,1,16,'#4D4D4D');
+}
