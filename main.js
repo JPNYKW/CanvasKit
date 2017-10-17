c3ecea976ffcb376f8e38cc270091c98368b68d1
JPNYKW <31243896+JPNYKW@users.noreply.github.com>
2017-10-17 18:58:55 +0900

    Update main.js

--- main.js
+++ main.js
@@ -51,7 +51,7 @@ document.onkeydown=e=>{
 				$E.push(prompt('Enter x pos'));
 				$E.push(prompt('Enter y pos'));
 				$E.push(prompt('Enter grid size'));
-				$E.push(prompt('Enter grid grids'));
+				$E.push(prompt('Enter grids number'));
 				$E.push(prompt('Enter bold'));
 				$E.push(prompt('Enter color (hex)'));
 				drawGrid(cont,$E[0],$E[1],$E[2],$E[3],$E[4],$E[5]);
