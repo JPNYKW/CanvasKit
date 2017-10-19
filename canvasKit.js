e9fd997934b75a402e1a1855d7380fc8c0bd84f0
JPNYKW <31243896+JPNYKW@users.noreply.github.com>
2017-10-19 18:53:21 +0900

    drawText関数の追加

--- canvasKit.js
+++ canvasKit.js
@@ -39,3 +39,12 @@ function drawFillBox(cont,x,y,size,color){
 	cont.fillStyle=color;
 	cont.fillRect(x-size/2,y-size/2,size,size);
 }
+
+function drawText(cont,text,x,y,font,bold,size,color,center){
+	cont.beginPath();
+	cont.font=`${size}px ${font}`;
+	cont.strokeStyle=color;
+	cont.lineWidth=bold;
+	if(center)x-=text.length/2*(size/2)-size/2;
+	cont.strokeText(text,x,y);
+}
