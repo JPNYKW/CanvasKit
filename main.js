31f3bbf06da655f6f7a1d1bc5f80118717e0d846
JPNYKW <31243896+JPNYKW@users.noreply.github.com>
2017-10-25 16:30:07 +0900

    Update main.js

--- main.js
+++ main.js
@@ -12,16 +12,16 @@ window.onload=()=>{
 	
 	//CanvasKit Functions
 	
-	drawDot(cont,320,320,8,'#F3F3F3');
+	cont.drawDot(320,320,8,'#F3F3F3');
 
-	drawLine(cont,50,128,590,128,3,'#F3F3F3');
-	drawLine(cont,50,512,590,512,3,'#F3F3F3');
+	cont.drawLine(50,128,590,128,3,'#F3F3F3');
+	cont.drawLine(50,512,590,512,3,'#F3F3F3');
 
-	drawGrid(cont,320,320,32,5,5,3,'#F3F3F3');
-	drawGrid(cont,320,320,640,1,1,32,'#C5C5C5');
-	drawGrid(cont,320,320,640,1,1,16,'#4D4D4D');
+	cont.drawGrid(320,320,32,5,5,3,'#F3F3F3');
+	cont.drawGrid(320,320,640,1,1,32,'#C5C5C5');
+	cont.drawGrid(320,320,640,1,1,16,'#4D4D4D');
 	
-	drawText(cont,'CanvasKit.js',320,500,'Arial',2,50,'#EEE',true,true);
+	cont.drawText('CanvasKit.js',330,500,'Arial',2,50,'#EEE',true,true);
 }
 
 // For demonstoration
@@ -38,7 +38,7 @@ document.onkeydown=e=>{
 				$E.push(prompt('Enter y pos'));
 				$E.push(prompt('Enter size'));
 				$E.push(prompt('Enter color (hex)'));
-				drawDot(cont,~~$E[0],~~$E[1],~~$E[2],$E[3]);
+				cont.drawDot(~~$E[0],~~$E[1],~~$E[2],$E[3]);
 			break;
 			case 'LINE':
 				$E.push(prompt('Enter start x pos'));
@@ -47,7 +47,7 @@ document.onkeydown=e=>{
 				$E.push(prompt('Enter end y pos'));
 				$E.push(prompt('Enter bold'));
 				$E.push(prompt('Enter color (hex)'));
-				drawLine(cont,~~$E[0],~~$E[1],~~$E[2],~~$E[3],~~$E[4],$E[5]);
+				cont.drawLine(~~$E[0],~~$E[1],~~$E[2],~~$E[3],~~$E[4],$E[5]);
 			break;
 			case 'GRID':
 				$E.push(prompt('Enter x pos'));
@@ -58,7 +58,7 @@ document.onkeydown=e=>{
 				$E.push(prompt('Enter bold'));
 				$E.push(prompt('Enter color (hex)'));
 				if($E[$E.length-1].substr(0,1)!=='#'){$E[$E.length-1]=`#${$E[$E.length-1]}`}
-				drawGrid(cont,~~$E[0],~~$E[1],~~$E[2],~~$E[3],~~$E[4],~~$E[5],$E[6]);
+				cont.drawGrid(~~$E[0],~~$E[1],~~$E[2],~~$E[3],~~$E[4],~~$E[5],$E[6]);
 			break;
 			case 'BOX':
 				$E.push(prompt('Enter x pos'));
@@ -66,14 +66,14 @@ document.onkeydown=e=>{
 				$E.push(prompt('Enter size'));
 				$E.push(prompt('Enter bold'));
 				$E.push(prompt('Enter color'));
-				drawBox(cont,~~$E[0],~~$E[1],~~$E[2],~~$E[3],$E[4]);
+				cont.drawBox(~~$E[0],~~$E[1],~~$E[2],~~$E[3],$E[4]);
 			break;
 			case 'FILLBOX':
 				$E.push(prompt('Enter x pos'));
 				$E.push(prompt('Enter y pos'));
 				$E.push(prompt('Enter size'));
 				$E.push(prompt('Enter color'));
-				drawFillBox(cont,~~$E[0],~~$E[1],~~$E[2],$E[3]);
+				cont.drawFillBox(~~$E[0],~~$E[1],~~$E[2],$E[3]);
 			break;
 			case 'TEXT':
 				$E.push(prompt('Enter text'));
@@ -83,7 +83,7 @@ document.onkeydown=e=>{
 				$E.push(prompt('Enter bold'));
 				$E.push(prompt('Enter size'));
 				$E.push(prompt('Enter color'));
-				drawText(cont,$E[0],~~$E[1],~~$E[2],$E[3],~~$E[4],~~$E[5],~~$E[6],true,true);
+				cont.drawText($E[0],~~$E[1],~~$E[2],$E[3],~~$E[4],~~$E[5],~~$E[6],true,true);
 			break;
 		}
 	}
