af996c2f896e0f006e31a025f5948ca4d66e4180
JPNYKW <31243896+JPNYKW@users.noreply.github.com>
2017-10-25 16:26:25 +0900

    Big update. Change function to prototype

--- canvasKit.js
+++ canvasKit.js
@@ -1,51 +1,53 @@
-function drawDot(cont,x,y,size,color){
-	cont.beginPath();
-	cont.fillStyle=color;
-	cont.arc(x,y,size,Math.PI*2,false);
-	cont.fill();
+CanvasRenderingContext2D.prototype.drawDot=function(x,y,size,color){
+	this.beginPath();
+	this.fillStyle=color;
+	this.arc(x,y,size,Math.PI*2,false);
+	this.fill();
 }
 
-function drawLine(cont,x,y,x1,y1,bold,color){
-	cont.beginPath();
-	cont.lineWidth=bold;
-	cont.strokeStyle=color;
-	cont.moveTo(x,y);
-	cont.lineTo(x1,y1);
-	cont.closePath();
-	cont.stroke();
+CanvasRenderingContext2D.prototype.drawLine=function(x,y,x1,y1,bold,color){
+	this.beginPath();
+	this.lineWidth=bold;
+	this.strokeStyle=color;
+	this.moveTo(x,y);
+	this.lineTo(x1,y1);
+	this.closePath();
+	this.stroke();
 }
 
-function drawGrid(cont,x,y,size,width,height,bold,color){
+
+
+CanvasRenderingContext2D.prototype.drawGrid=function(x,y,size,width,height,bold,color){
 	let drawX,drawY;
 	drawY=y-height/2*size+size/2;
 	for(let i=0;i<height;i++){
 		drawX=x-width/2*size+size/2;
 		for(let j=0;j<width;j++){
-			drawBox(cont,drawX,drawY,size,bold,color);
+			this.drawBox(drawX,drawY,size,bold,color);
 			drawX+=size;
 		}
 		drawY+=size;
 	}
 }
 
-function drawBox(cont,x,y,size,bold,color){
-	cont.beginPath();
-	cont.lineWidth=bold;
-	cont.strokeStyle=color;
-	cont.strokeRect(x-size/2,y-size/2,size,size);
+CanvasRenderingContext2D.prototype.drawBox=function(x,y,size,bold,color){
+	this.beginPath();
+	this.lineWidth=bold;
+	this.strokeStyle=color;
+	this.strokeRect(x-size/2,y-size/2,size,size);
 }
 
-function drawFillBox(cont,x,y,size,color){
-	cont.beginPath();
-	cont.fillStyle=color;
-	cont.fillRect(x-size/2,y-size/2,size,size);
+CanvasRenderingContext2D.prototype.drawFillBox=function(x,y,size,color){
+	this.beginPath();
+	this.fillStyle=color;
+	this.fillRect(x-size/2,y-size/2,size,size);
 }
 
-function drawText(cont,text,x,y,font,bold,size,color,center,fill){
-	cont.beginPath();
-	cont.font=`${size}px ${font}`;
-	fill?cont.fillStyle=color:cont.strokeStyle=color;
-	cont.lineWidth=bold;
+CanvasRenderingContext2D.prototype.drawText=function(text,x,y,font,bold,size,color,center,fill){
+	this.beginPath();
+	this.font=`${size}px ${font}`;
+	fill?this.fillStyle=color:this.strokeStyle=color;
+	this.lineWidth=bold;
 	if(center){x-=text.length/2*(size/2);}
-	fill?cont.fillText(text,x,y):cont.strokeText(text,x,y);
+	fill?this.fillText(text,x,y):this.strokeText(text,x,y);
 }
